8838d01f3bf12cc295eb6e28e066d6067cbbc218
Rebecca Uranga <urangarebecca@gmail.com>
2019-02-20 10:16:23 -0500

    updates instructor to set the name attribute

--- character.py
+++ character.py
@@ -7,4 +7,8 @@
 #   pass
 
 class Character(): # class names start with capital letter and are one word
-    pass
+    # the "dunder init" method is the constructor
+    def __init__(self, new_name):
+        # `self` is the customary way to refer to the instance being built
+        # In some other language, they use `this`
+        self.name = new_name

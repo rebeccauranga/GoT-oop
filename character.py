bb1af7416ed7eef5ff415f55e11a65c7a2936aa7
Rebecca Uranga <urangarebecca@gmail.com>
2019-02-20 11:20:16 -0500

    adds the avatar attribute and tests it

--- character.py
+++ character.py
@@ -8,7 +8,8 @@
 
 class Character(): # class names start with capital letter and are one word
     # the "dunder init" method is the constructor
-    def __init__(self, new_name):
+    def __init__(self, new_name, new_avatar):
         # `self` is the customary way to refer to the instance being built
         # In some other language, they use `this`
         self.name = new_name
+        self.avatar = new_avatar
